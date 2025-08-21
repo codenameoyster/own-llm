7fc7ce8ed1c476f38d14f3d93038fc1952cb0a09
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-08-21 21:01:38 +0200

    chore: better tokenization
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from tokenizer.tokenizer import download_file, tokenize_text
+from tokenizer.tokenizer import SimpleTokenizerV1, download_file, tokenize_text
 
 _log = logging.getLogger(__name__)
 
@@ -16,6 +16,15 @@ def main():
     all_words = sorted(set(tokenized_text))
     vocab_size = len(all_words)
     _log.info(f"Vocabulary size: {vocab_size}")
+    text = (
+        "It's the last he painted, you know, Mrs. Gisburn said with pardonable pride."
+    )
+
+    tokenizer = SimpleTokenizerV1(vocab={tkn: i for i, tkn in enumerate(all_words)})
+    ids = tokenizer.encode(text)
+    _log.info(f"Encoded text: {ids}")
+    decoded_text = tokenizer.decode(ids)
+    _log.info(f"Decoded text: {decoded_text}")
 
 
 if __name__ == "__main__":
--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,14 +1,16 @@
 import logging
 import os
+import re
 import urllib.request
+from typing import Any
 
-_log = logging.getLogger(__name__)
+_log: logging.Logger = logging.getLogger(__name__)
 
 
 def download_file(destination: str) -> str:
     """Download a file from a URL to a specified destination."""
     if not os.path.exists(destination):
-        url = ("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt")
+        url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
         _log.info(f"Downloading {url} to {destination}")
         try:
             urllib.request.urlretrieve(url, destination)
@@ -19,20 +21,28 @@ def download_file(destination: str) -> str:
     else:
         _log.info(f"File {destination} already exists. Skipping download.")
 
-    with open(destination, 'r', encoding='utf-8') as file:
+    with open(destination, "r", encoding="utf-8") as file:
         return file.read()
 
 
-def tokenize_text(text: str) -> list:
+def tokenize_text(text: str) -> list[str]:
     """Tokenize the input text into words."""
-    import re
+
     result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
     return [item.strip() for item in result if item.strip()]
 
 class SimpleTokenizerV1:
-    def __init__(self, vocab: dict):
-        self.str_to_init: dict = vocab
-        self.int_to_str = {i: s for s, i in vocab.items()}
-    def encode(self, text: str) -> list:
+    def __init__(self, vocab: dict[str, Any]) -> None:
+        self.str_to_int: dict[str, int] = vocab
+        self.int_to_str: dict[int, str] = {i: s for s, i in vocab.items()}
+
+    def encode(self, text: str) -> list[int]:
         """Convert text to a list of integers based on the vocabulary."""
-        return []
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        return [self.str_to_int[s] for s in preprocessed]
+
+    def decode(self, ids: list[int]) -> str:
+        """Convert a list of integers back to text."""
+        text: str = " ".join(self.int_to_str[i] for i in ids)
+        return re.sub(r'\s+([,.?!"()\'])', r"\1", text)
